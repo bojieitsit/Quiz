5dbaa4ca234f50e935e025bb787253da854553d1
Andrey Bogoslovskiy <88778576+bojieitsit@users.noreply.github.com>
2023-02-12 17:25:38 +0400

    Added 2 more variations of answers

--- QuizApp/Controller/ViewController.swift
+++ QuizApp/Controller/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    @IBOutlet weak var choice3Button: UIButton!
     
     var quizBrain = QuizBrain()
     var timer = Timer()
@@ -31,21 +32,27 @@ class ViewController: UIViewController {
         
         if userIsRight {
             sender.backgroundColor = .green
-            scoreLabel.text = "Score: \(quizBrain.scoreNumber)"
+            scoreLabel.text = "Score: \(quizBrain.score)"
         } else {
             sender.backgroundColor = .red
-            scoreLabel.text = "Score: \(quizBrain.scoreNumber)"
+            scoreLabel.text = "Score: \(quizBrain.score)"
         }
+        
+        quizBrain.nextQuestion()
         
         timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
     }
     
     @objc func updateUI() {
+        choice1Button.setTitle(quizBrain.getButtonTitle()[0], for: .normal)
+        choice2Button.setTitle(quizBrain.getButtonTitle()[1], for: .normal)
+        choice3Button.setTitle(quizBrain.getButtonTitle()[2], for: .normal)
+        choice1Button.backgroundColor = .clear
+        choice2Button.backgroundColor = .clear
+        choice3Button.backgroundColor = .clear
         progressBar.progress = quizBrain.getProgress()
         questionLabel.text = quizBrain.getQuestionText()
-        quizBrain.updateQuestion()
-        trueButton.backgroundColor = .clear
-        falseButton.backgroundColor = .clear
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
     }
     
 }
